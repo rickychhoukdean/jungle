ba0b1f6de6b872547517fe5e3343540e5e8f2b16
Ricky <chhoukdean184@msn.com>
2019-09-19 17:12:47 +0000

    Setup base routes

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,21 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["username"], password: ENV["password"]
+
+  def index
+    @products = Product.order(id: :desc).all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to [:admin, :products], notice: "Product created!"
+    else
+      render :new
+    end
+  end
 end
