06805887e8af3c9247e3d5313a67504842573e95
Ricky <chhoukdean184@msn.com>
2019-09-19 17:35:09 +0000

    Finish general functionality of categories like adding and viewing categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -13,9 +13,17 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :category], notice: "Category created!"
+      redirect_to [:admin, :categories], notice: "Category created!"
     else
       render :new
     end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
